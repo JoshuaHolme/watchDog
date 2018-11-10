4b96f49bf67fdda6e4b88cdcdb8f66d26ce655a1
Josh Holme <joshuaholme19@icloud.com>
2018-11-10 18:24:04 -0500

    Removed the "cancel" button after starting the mock workout

--- WatchDog WatchKit App/Controllers/InfoIC.swift
+++ WatchDog WatchKit App/Controllers/InfoIC.swift
@@ -1,31 +1,32 @@
 //
-//  InterfaceController.swift
-//  WatchDog WatchKit Extension
+//  InfoIC.swift
+//  WatchDog
 //
-//  Created by Sam DuBois on 11/10/18.
+//  Created by Joshua Holme on 11/10/18.
 //  Copyright © 2018 Holme Computer. All rights reserved.
 //
 
 import WatchKit
 import Foundation
 
-
-class InterfaceController: WKInterfaceController {
-
-    override func awake(withContext context: Any?) {
+class InfoIC: WKInterfaceController
+{
+    override func awake(withContext context: Any?)
+    {
         super.awake(withContext: context)
         
         // Configure interface objects here.
     }
     
-    override func willActivate() {
+    override func willActivate()
+    {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
     
-    override func didDeactivate() {
+    override func didDeactivate()
+    {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
 }
